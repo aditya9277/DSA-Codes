5f25fa4dbe371b7c42106c4d407c931466faee67
aditya9277 <adisamarth20@gmail.com>
2023-12-08 01:39:20 +0530

    code

---
+++ Arrays/arraylinearsearch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Enter element to be found : ";
+    cin >> x;
+    int arr[5] = {3, 6, 8, 3, 5};
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] == x)
+        {
+            // cout<<"Element found";
+            cout << "Found";
+        }
+    }
+
+    return 0;
+}
