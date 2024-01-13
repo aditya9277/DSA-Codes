477aa5806e4e4342eda151d93a97fcbe981c219d
aditya9277 <adisamarth20@gmail.com>
2024-01-14 00:18:41 +0530

    push

---
+++ Arrays/sort0and1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int arr[9]={1,0,0,1,1,1,0,1,0};
+    int n=9;
+    int right=n-1;
+    int left=0;
+    // for(int i=0;i<=right;i++){
+    //     if(arr[i]==0){
+    //         swap(arr[left],arr[i]); 
+    //         left++;
+
+    //     } 
+
+    //     if(arr[i]==1){
+    //         swap(arr[right],arr[i]); 
+    //         right--;
+    //     }
+    // } 
+
+    // }
+    int i=0;
+    while(i<=right){
+        if(arr[i]==0){
+            swap(arr[left],arr[i]);
+            i++; 
+            left++;
+        }
+        if(arr[i]==1){
+            swap(arr[right],arr[i]); 
+            right--;
+        }  
+    }
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
--- Arrays/tempCodeRunnerFile.cpp
+++ Arrays/tempCodeRunnerFile.cpp
@@ -1,12 +1,1 @@
-int count = 0;
-    // while (int i = 0 < (n - 1))
-    // {
-    //     for (int j = i + 1; j < n; j++)
-    //     {
-    //         if (nums[i] == nums[j])
-    //         {
-    //             count++;
-    //         }
-    //     }
-    //     i++;
-    // }
+            // right--;
