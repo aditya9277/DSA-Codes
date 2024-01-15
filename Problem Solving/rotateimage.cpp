75df4118dfe06e88c2ee1ab69d279b8644861c41
aditya9277 <adisamarth20@gmail.com>
2024-01-16 03:20:38 +0530

    push

--- Problem Solving/rotateimage.cpp
+++ Problem Solving/rotateimage.cpp
@@ -9,11 +9,12 @@ int main(){
     for(int i=0;i<3-1-i;i++){
         swap(mat[i],mat[3-1-i]);
     }
+    //no need to do temp method, rather  use i+1 in j;
     for(int i=0;i<3;i++){
-        for(int j=i;j<3;j++){
-            int temp=mat[i][j];
-            mat[i][j]=mat[j][i];
-            mat[j][i]=temp;
+        for(int j=i+1;j<3;j++){
+            // int temp=mat[i][j];
+            swap(mat[i][j],mat[j][i]);
+            // mat[j][i]=temp;
         }
     }
 
