d74fa21841b38055c13a517225229f1a20f9b2ed
aditya9277 <adisamarth20@gmail.com>
2024-01-15 16:30:38 +0530

    push

--- Arrays/2Darray.cpp
+++ Arrays/2Darray.cpp
@@ -6,14 +6,15 @@ int main(){
     int arr[3][3];
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            cin>>arr[j][i];
+            cin>>arr[i][j];
         }
     }
     for(int i=0;i<3;i++){
+        int sum=0;
         for(int j=0;j<3;j++){
-            cout<<arr[j][i]<<" ";
+            sum+=arr[i][j];
         }
-        cout<<endl;
+        cout<<sum<<endl;
     }
     return 0;
 }
