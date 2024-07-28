25c930d9a294c6110eee755b835861340678abbc
aditya9277 <adisamarth20@gmail.com>
2024-07-28 10:59:00 +0530

    code

--- Pointers/1.cpp
+++ Pointers/1.cpp
@@ -6,14 +6,24 @@ using namespace std;
 
 
 int main(){
-    int a=5;
-    int *p = &a;
-    // cout<<*p<<endl;
-    cout<<sizeof(p)<<endl;
+    // int a=5;
+    // int *p = &a;
+    // // cout<<*p<<endl;
+    // cout<<sizeof(p)<<endl;
 
-    char c='a';
-    char *ch=&c; 
-    cout<<sizeof(ch);
+    // char c='a';
+    // char *ch=&c; 
+    // cout<<sizeof(ch);
+
+    int arr[5]={0,1,2,3,4};
+    cout<<arr<<endl;
+    cout<<&arr<<endl;
+    cout<<&arr[0]<<endl;
+    int *p= arr;
+    cout<<p<<endl;
+    cout<<&p<<endl;
+
+
 
 
 
